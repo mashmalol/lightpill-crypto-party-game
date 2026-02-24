673b7208e790df8ef0069f959c81452fe4fee435
mashmalol <adnan.steve2@gmail.com>
2026-02-24 13:09:24 -0500

    Use relative path for entry point - fixes Vercel resolution

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
 })
 
